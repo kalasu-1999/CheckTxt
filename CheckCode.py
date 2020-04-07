54c2652b9de5292b32283f8f8648e4d32561cfa8
kalasu-1999 <git@github.com:kalasu-1999/Java.git>
2020-04-07 20:26:48 +0800

    demo03

--- CheckCode.py
+++ CheckCode.py
@@ -5,6 +5,7 @@
 def clean(line, num):
     while line[num].strip() == '':
         num = num + 1
+    return num
 
 
 def getLine(path):
@@ -18,27 +19,33 @@ def logic(c):
         return False
     # 中间行
     else:
-		temp1 = c.trueNum
-		temp2 = c.falseNum
-		clean(c.trueLine, temp1)
-		clean(c.falseLine, temp2)
-        if c.trueLine[temp1 + 1] == c.falseLine[temp2 + 1]:
-            return True
-        else:
-            return False
-
-
-# 多出了一些行
-# 未完成
-def overTrue(c):
-    
-	return 0
+        temp1 = c.trueNum
+        temp2 = c.falseNum
+        clean(c.trueLine, temp1)
+        clean(c.falseLine, temp2)
+    if c.trueLine[temp1 + 1] == c.falseLine[temp2 + 1]:
+        return True
+    else:
+        return False
 
 
 # 少了一些行
 def lessTrue(c):
-    
-	return 0 
+    temp1 = c.falseNum + 1
+    temp1 = clean(c.falseLine, temp1)
+    temp2 = c.trueNum + 1
+    temp2 = clean(c.trueLine, temp2)
+    temp3 = temp2 + 1
+    temp3 = clean(c.trueLine, temp3)
+    while temp3 != c.trueLine:
+        if c.trueLine[temp2] == c.falseLine[c.falseNum] and c.trueLine[temp3] == c.falseLine[temp1]:
+            return temp2
+        else:
+            temp2 = temp2 + 1
+            temp2 = clean(c.trueLine, temp2)
+            temp3 = temp2 + 1
+            temp3 = clean(c.trueLine, temp3)
+    return -1
 
 
 def check(c):
@@ -55,27 +62,28 @@ def check(c):
         else:
             # 判断是否为逻辑错误
             if logic(c):
-                answerDir[c.falseNum + 1] = {"单行逻辑错误"}
+                answerDir[c.falseNum + 1] = {0}
+                c.trueNum = c.trueNum + 1
+                c.falseNum = c.falseNum + 1
             else:
-				overReturn = overTrue(c)
-				if overReturn != 0:
-					answerDir[c.falseNum + 1] = {"相比于源代码多出了一些行，到" + overReturn + "行"}
-					c.falseNum = overReturn + 1
-					c.trueNum = c.trueNum + 1
-				lessReturn = lessTrue(c)
-				if lessReturn != 0:
-					answerDir[c.falseNum + 1] = {"相比于源代码少了一些行，共少了" + (c.trueNum - lessReturn) + "行"}
-					c.trueNum = lessReturn + 1
-					c.falseNum = c.falseNum + 1
-	# 运行到最后有剩余行
-	# 正确程序还有剩余行
-	if c.trueNum == c.trueLine.__len__() and c.falseNum != c.falseLine.__len__():
-		answerDir[c.falseNum + 1] = {"此行开始至末尾，源程序代码中不存在"}
-	elif c.falseNum == c.falseLine.__len__() and c.trueNum != c.trueLine.__len__():
-		answerDir[c.falseNum + 1] = {"源程序代码中还未结束，缺少代码"}
+                lessReturn = lessTrue(c)
+                if lessReturn != 0:
+                    answerDir[c.falseNum + 1] = {lessReturn - c.trueNum}
+                    c.trueNum = lessReturn + 1
+                    c.falseNum = c.falseNum + 1
+    # 运行到最后有剩余行
+    # 正确程序还有剩余行
+    if c.trueNum == c.trueLine.__len__() and c.falseNum != c.falseLine.__len__():
+        answerDir[c.falseNum + 1] = {-1}
+    elif c.falseNum == c.falseLine.__len__() and c.trueNum != c.trueLine.__len__():
+        answerDir[c.falseNum + 1] = {-1}
+    print(answerDir)
 
 
 class CheckCode:
+    def __init__(self):
+        pass
+
     trueLine = []
     falseLine = []
     trueNum = 0
@@ -84,8 +92,6 @@ class CheckCode:
 
 if __name__ == '__main__':
     ck = CheckCode()
-    ck.trueLine = getLine(
-        "/mnt/e2ae2387-deae-49e8-bbbc-d48d4ca5897d/MyData/创新实践/CodeCheckTextDir/OldFile")
-    ck.falseLine = getLine(
-        "/mnt/e2ae2387-deae-49e8-bbbc-d48d4ca5897d/MyData/创新实践/CodeCheckTextDir/NewFile")
+    ck.trueLine = getLine("OldFile")
+    ck.falseLine = getLine("NewFile")
     check(ck)
